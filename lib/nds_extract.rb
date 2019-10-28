d649e8ab8294e5461bf1667ed0468fc0c614e958
Alex Suthammanont <asuthamm@gmail.com>
2019-10-28 20:42:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,21 +29,21 @@ def list_of_directors(source)
 
   i=0 
   while directors.length < source.length
-    directors.push(source)
+    directors.push(source[i][:name])
     i += 1
   end 
-  
+  directors
 
 
-{"Stephen Spielberg"	=>1357566430,
- "Russo Brothers"		=>2281002470,
- "James Cameron"		=>2571020373,
- "Spike Lee"		=>256624217,
- "Wachowski Siblings"	=>806180282,
- "Robert Zemeckis"		=>1273838385,
- "Quentin Tarantino"	=>662738268,
- "Martin Scorsese"		=>636812242,
- "Francis Ford Coppola"	=>509719258}
+# {"Stephen Spielberg"	=>1357566430,
+# "Russo Brothers"		=>2281002470,
+# "James Cameron"		=>2571020373,
+# "Spike Lee"		=>256624217,
+# "Wachowski Siblings"	=>806180282,
+# "Robert Zemeckis"		=>1273838385,
+# "Quentin Tarantino"	=>662738268,
+# "Martin Scorsese"		=>636812242,
+# "Francis Ford Coppola"	=>509719258}
 
 
 

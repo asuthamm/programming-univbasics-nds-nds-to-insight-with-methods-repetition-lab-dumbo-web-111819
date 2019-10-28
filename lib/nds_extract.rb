40a417f709d61a390d0303c4e626e748b41f966e
Alex Suthammanont <asuthamm@gmail.com>
2019-10-28 20:44:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,15 +24,6 @@ def gross_for_director(d)
   total
 end
 
-def list_of_directors(source)
-  directors = []
-
-  i=0 
-  while directors.length < source.length
-    directors.push(source[i][:name])
-    i += 1
-  end 
-  directors
 
 
 # {"Stephen Spielberg"	=>1357566430,
